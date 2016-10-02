802182b3d927511fdcdebfe1840c09161ca42734
jca322 <jca322@gmail.com>
2016-10-02 12:18:18 -0400

    Add append function using loop

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var x = {value: 1, next:{value: 2, next:{value: 3, next: null}}};
 
+//Print all values in linked list using recursion
 var print = function(node) {
     if(node === null) {
         return;
@@ -8,4 +9,28 @@ var print = function(node) {
     print(node.next);
 };
 
+//Print all values in linked list using a loop
+var print = function(node) {
+    var cur = node;
+    while (cur !== null) {
+        console.log(cur.value);
+        cur = cur.next;
+    }
+};
+
+//Add a new node to the end of linked list using a loop
+var append = function(node, value) {
+    var cur = node;
+    while (cur.next !== null) {
+        cur = cur.next;
+    }
+    cur.next = {value: value, next: null};
+};
+
+
+console.log("initial list");
+print(x);
+
+console.log("append new value to list");
+append(x, 7);
 print(x);
