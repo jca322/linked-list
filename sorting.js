f2b0180200039ed6625f16e847ce8640fddabf2a
jca322 <jca322@gmail.com>
2016-10-03 21:49:10 -0400

    Add function to sort an array using an insertion sort

--- sorting.js
+++ sorting.js
@@ -5,7 +5,7 @@ var swap = function(arr, i, j) {
     arr[j] = temp;
 };
 
-//Sort an array selection sort
+//Sort an array using a selection sort
 var sort = function(arr) {
     for(var i = 0; i < arr.length; i++) {
         var smallest = arr[i];
@@ -20,6 +20,25 @@ var sort = function(arr) {
     }  
 };
 
+//Sort an array using an insertion sort
+var insert = function(arr) {
+    for(var i = 1; i < arr.length; i++) {
+        for(var j = i; j > 0; j--) {
+            if(arr[j] > arr[j-1]) {
+                break;
+            }
+            swap(arr, j, (j-1));
+        }
+    }
+};
+
+console.log("return sorted array");
 var arr = [7, 4, 19, 6, 22, 2];
 sort(arr);
 console.log(arr);
+
+console.log("insert value into sorted array");
+var arr2 = [4, 32, 6, 1, 17, 5, 7];
+insert(arr2);
+console.log(arr2);
+
